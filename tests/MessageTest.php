e7aa234bc35e6d3c13b17d2efb75e9bf913b9449
Hugo Hirsch <github@hugo-hirsch.de>
2019-03-02 23:30:24 +0100

    Issue #3: Move to pkg mailform

--- tests/MessageTest.php
+++ tests/MessageTest.php
@@ -1,8 +1,8 @@
 <?php
 declare(strict_types=1);
 
-use Mailform\Email;
-use Mailform\Message;
+use mailform\Email;
+use mailform\Message;
 use PHPUnit\Framework\TestCase;
 
 final class MessageTest extends TestCase
