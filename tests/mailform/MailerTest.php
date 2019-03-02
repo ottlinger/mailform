9030c5cbb665dc5739d796861a956ed9d53d5fae
Hugo Hirsch <github@hugo-hirsch.de>
2019-03-03 00:30:53 +0100

    Issue #3: Add test

--- tests/mailform/MailerTest.php
+++ tests/mailform/MailerTest.php
@@ -1,18 +1,16 @@
 <?php
 declare(strict_types=1);
 
-use mailform\Email;
+use mailform\Mailer;
 use mailform\Message;
 use PHPUnit\Framework\TestCase;
 
 final class MailerTest extends TestCase
 {
-    public function testObjectCreationAndGetters()
+    public function testCreationWithExplicitSendOut()
     {
         $message = new Message("MyName   ", " ÄMyContents", "foo@bar.com ");
-        $this->assertEquals("MyName", $message->getName());
-        $this->assertEquals("ÄMyContents", $message->getContents());
-        $this->assertEquals("foo@bar.com", $message->getEmail());
-        $this->assertTrue($message->isValid());
+        $mailer = new Mailer($message, true);
+        $this->assertTrue($mailer->isSendOut());
     }
 }
