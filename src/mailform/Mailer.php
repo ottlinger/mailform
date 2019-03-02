5c59e53e456978e7fb52b40b4b5e718bcc9c28f0
Hugo Hirsch <github@hugo-hirsch.de>
2019-03-03 00:22:23 +0100

    Issue #3: Add new mailer class

---
+++ src/mailform/Mailer.php
@@ -0,0 +1,15 @@
+<?php
+namespace mailform;
+
+final class Mailer
+{
+    private $message;
+    private $sendOut;
+
+    public function __construct(Message $message, $sendOut = false)
+    {
+        $this->message = $message;
+        $this->sendOut = $sendOut;
+    }
+
+}
---
+++ tests/mailform/MailerTest.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+use mailform\Email;
+use mailform\Message;
+use PHPUnit\Framework\TestCase;
+
+final class MailerTest extends TestCase
+{
+    public function testObjectCreationAndGetters()
+    {
+        $message = new Message("MyName   ", " ÄMyContents", "foo@bar.com ");
+        $this->assertEquals("MyName", $message->getName());
+        $this->assertEquals("ÄMyContents", $message->getContents());
+        $this->assertEquals("foo@bar.com", $message->getEmail());
+        $this->assertTrue($message->isValid());
+    }
+
+    public function testObjectWithInvalidName()
+    {
+        $message = new Message("", " ÄMyContents", "foo@bar.com ");
+        $this->assertFalse($message->isValid());
+    }
+
+    public function testObjectWithInvalidContents()
+    {
+        $message = new Message("YourName", "", "foo@bar.com ");
+        $this->assertFalse($message->isValid());
+    }
+
+    public function testObjectWithInvalidMailAddress()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        new Message("YourName", "MyContents", "invalid");
+    }
+
+}
