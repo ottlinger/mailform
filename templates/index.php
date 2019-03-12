63926c710f6ddd1fed26fb94f50137f06f7ca08f
Hugo Hirsch <github@hugo-hirsch.de>
2019-03-12 14:53:49 +0100

    Add lang attribute

--- templates/index.php
+++ templates/index.php
@@ -179,7 +179,7 @@ $sendOut = false;
 
                         if ($sendOut) {
                             print "<h4>Thanks for submitting your request at " . date('Y-m-d H:i:s') . "</h4>";
-                            print "<div class=\"col-12\"><p>You may return to our >>> <a href=\"" . Mailer::getFromConfiguration("successlinktarget") . "\">main application page</a></p></div>";
+                            print "<div class=\"col-12\"><p>You may return to our >>> <a href=\"" . Mailer::getFromConfiguration('successlinktarget') . "\">main application page</a></p></div>";
                         }
                         ?>
                     </div>
