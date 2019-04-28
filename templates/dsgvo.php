fc2633fbe7b2096412d742b6e14a2cf67d0fdb38
Hugo Hirsch <github@hugo-hirsch.de>
2019-04-28 20:24:55 +0200

    Issue #10: Add back links from templates

--- templates/dsgvo.php
+++ templates/dsgvo.php
@@ -4,4 +4,8 @@ echo "<h1>GDPR - please adapt</h1>";
 echo "Your GDPR contents goes here :-)<br>";
 echo date("Y-m-d");
 
-
+?>
+<br />
+<hr />
+<br />
+<a href="./">return to application</a>
--- templates/imprint.php
+++ templates/imprint.php
@@ -4,4 +4,8 @@ echo "<h1>Imprint - please adapt</h1>";
 echo "Your imprint contents goes here :-)<br>";
 echo date("Y-m-d");
 
-
+?>
+<br />
+<hr />
+<br />
+<a href="./">return to application</a>
