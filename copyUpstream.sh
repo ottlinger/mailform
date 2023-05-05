5f3804cd73c498220690e2ac0074760f4888b8cf
Hugo Hirsch <github@hugo-hirsch.de>
2023-05-05 21:32:06 +0200

    Fix codacy issue

--- copyUpstream.sh
+++ copyUpstream.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Will upload stuff from current directory to hoster ..... "
-scp -O -r * 34021f23452@hugo-hirsch.de:~/htdocs/a_mailform/
+scp -O -r ./* 34021f23452@hugo-hirsch.de:~/htdocs/a_mailform/
 
 echo "DATA SYNC finished"
 echo "In case you need to sync config changes continue or press Ctrl-C to abort."
