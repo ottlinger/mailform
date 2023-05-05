a7c5379f8372d7d3fc9c291f033395644abf8cf1
Hugo Hirsch <github@hugo-hirsch.de>
2023-05-05 21:28:54 +0200

    Add sync step for live config

--- copyUpstream.sh
+++ copyUpstream.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Will upload stuff from current directory to hoster ..... "
 scp -O -r * 34021f23452@hugo-hirsch.de:~/htdocs/a_mailform/
+
+echo "DATA SYNC finished"
+echo "In case you need to sync config changes continue or press Ctrl-C to abort."
+scp -O ../mailform-config-live.php 34021f23452@hugo-hirsch.de:~/htdocs/a_mailform/config/mailform-config.php
